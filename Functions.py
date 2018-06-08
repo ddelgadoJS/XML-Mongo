b41306335bd813c5fb73d0637b3b588363428bd9
Daniel Delgado <delgado.jdds@gmail.com>
2018-06-08 00:01:03 -0600

    Parser working (minor bugs)

--- Functions.py
+++ Functions.py
@@ -12,8 +12,7 @@ def center(win):
     win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
 # Reads and parses an XML file to JSON.
-# NOT WORKING
-def xmlParserJSON(filePath):
+def parseXMLtoJSON(filePath):
     with open(filePath, 'r') as f:
         xmlString = f.read()
 
@@ -29,3 +28,118 @@ def xmlParserJSON(filePath):
 
     with open(fileName + ".json", 'w') as f:
         f.write(jsonString)
+
+def updateXML(filePath, collectionName):
+    with open(filePath, 'r') as f:
+        xmlString = f.read()
+
+    tagContent = ""
+    xmlStringSimple = ""
+    tag = ""
+    reutersContent = ""
+    stack = []
+    #for i in range(1, len(xmlString)):
+    for i in xmlString:
+        # Gets tag
+        # If stack[-1] == '<' a tag is being parsed.
+        if i == '<' or (len(stack) > 0 and stack[-1] == '<'):
+            # Tag's start.
+            if len(stack) == 0 or stack[-1] != '<':
+                tag = "<"
+                stack.append(i) # Adds '<' to know it's parsing a tag.
+
+            # Tag's end.
+            elif i == '>':
+                stack.pop() # Pops '<'.
+                tag += i
+                # Checks if it's starting tag or ending tag.
+                if tag[1] == '/':
+                    if (tag == "</DATE>" or tag == "</TOPICS>" or
+                       tag == "</PLACES>" or tag == "</PEOPLE>" or
+                       tag == "</ORGS>" or tag == "</EXCHANGES>" or
+                       tag == "</TEXT>" or tag == "</TITLE>" or
+                       tag == "</AUTHOR>" or tag == "</DATELINE>" or
+                       tag == "</BODY>" or tag == "</REUTERS>" or
+                       tag == "</COLLECTION>"):
+
+                        # COLLECTION case: change name given by user.
+                        if tag == "</COLLECTION>":
+                            tag = '</' + collectionName + '>'
+
+                        elif tag == "</TITLE>":
+                            tagContent = tagContent[5:]
+
+                        elif tag == "</BODY>":
+                            tagContent = tagContent[:-4]
+
+                        xmlStringSimple += tagContent
+                        xmlStringSimple += tag
+
+                        if len(stack) > 0 and stack[-1] == tag[0] + tag[2:]: # Looks for ending tag in stack.
+                            stack.pop() # Pops opening tag.
+                            tagContent = ""
+
+                        else:
+                            print ("Error: opening tag not found.")
+
+                    else:
+                        tagContent = ""
+                        print ("Tag not added: " + tag)
+
+                # First line case.
+                elif tag[1] == '?':
+                    print ("First line ignored.")
+
+                else:
+                    # COLLECTION case: change name given by user.
+                    if tag == "<COLLECTION>":
+                        tag = '<' + collectionName + '>'
+                        xmlStringSimple += tag
+                        stack.append(tag)
+
+                    # Update xmlStringSimple
+                    elif (tag == "<DATE>" or tag == "<TOPICS>" or
+                       tag == "<PLACES>" or tag == "<PEOPLE>" or
+                       tag == "<ORGS>" or tag == "<EXCHANGES>" or
+                       tag == "<TEXT>" or tag == "<TITLE>" or
+                       tag == "<AUTHOR>" or tag == "<DATELINE>" or
+                       tag == "<BODY>" or tag == "<REUTERS>" or
+                       tag == "<COLLECTION>"):
+
+                        if tag == "<COLLECTION>":
+                           tag = '<' + collectionName + '>'
+
+                        xmlStringSimple += tag
+                        stack.append(tag)
+
+                    else:
+                        tagContent = ""
+                        print ("Tag not added: " + tag)
+                #print (stack)
+
+            # Already started parsing tag.
+            else:
+                # REUTERS case: only NEWID needed.
+                if tag == "<REUTERS":
+                    reutersContent += i
+
+                else:
+                    tag += i
+
+        # Gets content of tag
+        else:
+            tagContent += i
+
+    # Creates new simplified XML.
+    fileName = ((filePath.split("\\")[-1]).split("."))[0]
+    with open(fileName + "simpleXXX" + ".xml", 'w') as f:
+        f.write(xmlStringSimple)
+
+    parseXMLtoJSON(fileName + "simpleXXX" + ".xml")
+
+    print(xmlStringSimple)
+
+
+updateXML("C:\\Users\\Daniel\\Documents\\GitKraken\\XMLtoMongo\\reut2-000.xml", "ColeccionPrueba")
+
+#parseXMLtoJSON("reut2-000simpleXXX.xml")
--- Main.py
+++ Main.py
@@ -16,13 +16,18 @@ xmlFrame.pack(side = "top")
 collectionFrame.pack(side = "top")
 bottomFrame.pack(side = "top")
 
+xmlPathVar = StringVar()
+
 Label(frame, text = "Proyecto 3 - MongoDB", font = "Helvetica 15 bold").pack(side = "top", pady = 10) # Title
 Label(xmlFrame, text = "Dirección de archivo XML: ", font = "Helvetica 12").pack(side = "left", padx = 10, pady = 5)
 Label(collectionFrame, text = "Nombre de colección: ", font = "Helvetica 12").pack(side = "left", padx = 25, pady = 10)
-xmlPath = Entry(xmlFrame, font = "Helvetica 12")
+xmlPath = Entry(xmlFrame, font = "Helvetica 12", textvariable = xmlPathVar)
 xmlPath.pack(side = "right", padx = 5, pady = 5)
 collectionName = Entry(collectionFrame, font = "Helvetica 12")
 collectionName.pack(side = "right", padx = 5, pady = 10)
-Button(bottomFrame, text = "Cargar", font = "Helvetica 12", command = lambda: Funs.xmlParserJSON(xmlPath.get())).pack(side = "bottom", padx = 10, pady = 10)
+
+xmlPathVar.set("C:\\Users\\Daniel\\Documents\\GitKraken\\XMLtoMongo\\reut2-000-simple.xml")
+
+Button(bottomFrame, text = "Cargar", font = "Helvetica 12", command = lambda: Funs.parseXMLtoJSON(xmlPath.get())).pack(side = "bottom", padx = 10, pady = 10)
 
 win.mainloop()
